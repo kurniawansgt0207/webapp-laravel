a52b923e2955284dbf4a67674d48fb13330b6206
kurniawansgt0207 <58014838+kurniawansgt0207@users.noreply.github.com>
2025-03-13 14:03:08 +0700

    Update navbar.blade.php
    Mengubah menu Home menjadi Beranda

--- resources/views/_partials/navbar.blade.php
+++ resources/views/_partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="{{ route('index.home') }}">Home</a></li>
+        <li><a href="{{ route('index.home') }}">Beranda</a></li>
         <li><a href="{{ route('posts.list.component') }}">List Data (Component)</a></li>
         <li><a href="{{ route('posts.list') }}">List Data</a></li>
         <li><a href="{{ route('posts.input') }}">Input Data</a></li>
